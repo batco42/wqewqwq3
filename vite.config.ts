da7f8e2c46caa38aff3aff24a9e886a3e438174a
batco42 <ardakayhan048306@gmail.com>
2025-12-19 21:46:26 +0300

    batuco
    batco

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  root: ".",           // Proje kökü
+  base: "./",          // Build sırasında tüm asset yolları göreli olacak
   server: {
     host: "::",
     port: 8080,
